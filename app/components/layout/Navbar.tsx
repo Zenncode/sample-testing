c59ed8a665c61509b645f47a75953d13663377ab
Rendjie <mapagdalitarendjie37@gmail.com>
2026-03-25 17:32:26 +0800

    added chatbot

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         
         {/* Logo / Brand */}
         <h1 className="text-xl font-bold text-gray-800">
-          App ni ZenJan Gaming
+          App ni kuya ZenJan Gaming
         </h1>
 
         {/* Navigation Links */}
